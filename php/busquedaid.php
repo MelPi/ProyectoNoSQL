a71bd1740680cd5ecdab80028cf0e976fc279796
gcoayla <gcoayla.28@gmail.com>
2017-11-20 12:47:47 -0500

    Revert "Merge branch 'master' of https://github.com/MelPi/ProyectoNoSQL"
    This reverts commit 14a9cde7b8a6b5e598792eff627586a1a6d1e602, reversing
    changes made to e8a5420cbb171cf038bea37a3f3f3d1e55e7f5c8.

--- php/busquedaid.php
+++ php/busquedaid.php
@@ -2,7 +2,6 @@
 require '../../vendor/autoload.php';
 
 $collection = (new MongoDB\Client)->codenmicoledb->codenmicoledb;
-<<<<<<< HEAD
 $cursor = $collection->find(['documento' => '72378629']);
 
 foreach ($cursor as $document) {
@@ -15,12 +14,6 @@ foreach ($cursor as $document) {
            .$asis['tiempototal'];
    }
     
-=======
-$cursor = $collection->find(['documento' => '74085192']);
-
-foreach ($cursor as $document) {
-    echo $document['nombre'], "\n";
->>>>>>> c265facc2bcb01445d2e380b4c9901151368b2c0
 }
 
 ?>
